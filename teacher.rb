e9bbbde8d9082800691a159e5aa230732e9561e6
Moathal <moathal.s.kachi@gmail.com>
2022-11-08 19:25:41 +0300

    Fix rubocop error

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require_relative './person'
 
 class Teacher < Person
-  def initialize(age, specialization, name = "Unknown", parent_permission = true)
-    super(age, name, parent_permission = true)
+  def initialize(age, specialization, name = 'Unknown')
+    super(age, name)
     @specialization = specialization
   end
 
