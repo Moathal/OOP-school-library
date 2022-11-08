949493c691abc915920993124a72252d0536384b
Moathal <moathal.s.kachi@gmail.com>
2022-11-08 19:19:29 +0300

    Add arguments to super &  initialize of the children

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require_relative './person'
 
 class Teacher < Person
-  def initialize(specialization)
-    super
+  def initialize(age, specialization, name = "Unknown", parent_permission = true)
+    super(age, name, parent_permission = true)
     @specialization = specialization
   end
 
